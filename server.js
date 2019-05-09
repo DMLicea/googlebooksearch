9a7503c3af2a6afe388309f98d54a454db5f1ec8
danielle <dmlicea@gmail.com>
2019-05-08 22:53:45 -0500

    folders

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-require('dotenv').config();
+    
 const express = require("express");
+
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -11,31 +17,18 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
+// Add routes, both API and view
 
-const mongoose = require("mongoose");
+app.use(routes);
 
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
+// Connect to the Mongo DB
 
-mongoose.connect(mongoURL, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-  .then(() => {
-    
-    console.log(" ==> Successfully connected to mongoDB.");
+// Start the API server
 
-  })
-  
-  .catch((err) => {
-    
-    console.log('Error connecting to mongoDB');
-
-  });
-
-require("./routes/api-routes")(app);
-
-app.listen(PORT, () => {
-
-  console.log('🌎 ==> API server now on Port 3000');
-
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
