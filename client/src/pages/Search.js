a1cdaa4951ecb6496bd1d0665afec2b2e3cdeb33
danielle <dmlicea@gmail.com>
2019-05-09 02:56:24 -0500

    more tweaks to saved and search pages

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,1 +1,10 @@
+// import React, { Component } from "react";
+// import API from "../utils/API";
+// import {Row, Col} from "../Grid"
+//import { Link } from "react-router-dom";
+
+class Search 
+//extends Component 
+{}
+
 export default Search;
