5769b140961b50aee0c0cc37cb9007a59427d5f2
danielle <dmlicea@gmail.com>
2019-05-09 02:31:24 -0500

    search and saved

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Books from "./components/Books";
+import Search from "./pages/Search";
+
+import Saved from "./pages/Saved";
 
 import Jumbotron from "./components/Jumbotron";
 
@@ -22,9 +24,9 @@ function App() {
 
         <Switch>
 
-          <Route exact path="/" component={Books} />
+          <Route exact path="/" component={Search} />
 
-          <Route exact path="/books" component={Books} />
+          <Route exact path="/saved" component={Saved} />
 
         </Switch>
 
